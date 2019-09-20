86e9580dbd33f158c562ec207bfe52c5ccf0fec3
nakanaka320 <nakanakamitsuo@icloud.com>
2019-09-20 12:38:55 +0900

    ビューの実装を行いました

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,10 @@
+class MessagesController < ApplicationController
+  def index
+  end
+
+  def user
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'messages/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'messages#index'
+  get 'messages' => 'messages#user'
+  # create 'messages' => 'messages#new'
 end
