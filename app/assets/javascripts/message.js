5c24438c13cd8700c7478e9fc392e7e6c42df0a2
nakanaka320 <nakanakamitsuo@icloud.com>
2019-10-10 15:08:01 +0900

    非同期通信

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,37 @@
+$(function() {
+  function buildHTML(message) {
+    var html = `<div class="rightcontent__message">
+                    <div class="rightcontent__message__name">
+                      ${message.user_name}
+                    </div>
+                    <div class="rightcontent_time">
+                      ${message.time}
+                    </div>
+                    <p class="rightcontent__message__a">
+                      ${message.content}
+                    </p>`
+    return html;
+              };
+           
+$('#new_message').on('submit', function(e){
+  e.preventDefault();
+
+  var formData = new FormData(this);
+  var url = $(this).attr('action');
+
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+  })
+
+  .done(function(data) {
+    console.log(data)
+    var html = buildHTML(data);
+    $('.rightcontent__messageslist').append(html);
+  })
+  })
+});
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,4 +1,4 @@
-json.user.name @message.user.name
-json.time @massage.created_at.strftime("%Y/%m/%d %H:%M")
+json.user_name @message.user.name
+json.time @message.created_at.strftime("%Y/%m/%d %H:%M")
 json.content @message.content
 json.image @message.image.url
